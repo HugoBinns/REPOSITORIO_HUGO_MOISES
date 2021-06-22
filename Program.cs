7b3a6808a95259f54cc3cfb88a2101c106c97fe2
HugoBinns <52224327+HugoBinns@users.noreply.github.com>
2021-06-22 17:19:28 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,192 +1,95 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace proyecto3
 {
     class Program
     {
-        protected static int origRow;
-        protected static int origCol;
-        protected static void WriteAt(string s, int x, int y)
+
+
+        public static int Potencia(int a, int p)
         {
-            try
+            int i = 0;
+            int n = 1;
+
+            for (i = 1; i <= p; i++)
             {
-                Console.SetCursorPosition(origCol + x, origRow + y);
-                Console.Write(s);
+                n *= a;
             }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.Clear();
-                Console.WriteLine(e.Message);
-            }
+
+            return n;
         }
+        public static int permutacionSinRepeticion(int n)
+        {
+            int i, a;
+            a = 1;
+            for (i = 1; i <= n; i++)
+            {
+                a *= i;
+            }
+            return a;
+        }
+
+        public static float permutacionConRepeticion(int n, int a, int b, int c)
+        {
+            float p;
+            p = 0.0f;
+            p = permutacionSinRepeticion(n) / (permutacionSinRepeticion(a) * permutacionSinRepeticion(b) * permutacionSinRepeticion(c));
+            return p;
+        }
+
+        public static float Emparejamiento(int m, int n)
+        {
+            float e;
+            int a, b, c;
+            a = 1;
+            b = 1;
+            c = 1;
+            e = 0.0f;
+
+            a = permutacionSinRepeticion(m * n);
+            b = Potencia(permutacionSinRepeticion(m), n);
+            c = permutacionSinRepeticion(n);
+
+            e = a / (b * c);
+           
+            return e;
+        }
+
+        public static float variacionSinRepeticion(int n, int r)
+        {
+            float v;
+            v = 0.0f;
+            v = permutacionSinRepeticion(n) / permutacionSinRepeticion(n - r);
+            return v;
+        }
+
+        public static int variacionConRepeticion(int n, int r)
+        {
+            int v = 0;
+            v = Potencia(n, r);
+            return v;
+        }
+
+        public static float combinacionSinRepeticion(int n, int r)
+        {
+            float c = 0.0f;
+            c = permutacionSinRepeticion(n) / (permutacionSinRepeticion(r) * (permutacionSinRepeticion(n - r)));
+            return c;
+        }
+
+        public static float combinacionesConRepeticion(int n, int r)
+        {
+            float c = 0.0f;
+            c = permutacionSinRepeticion(n + r - 1) / permutacionSinRepeticion(r) * permutacionSinRepeticion(n - 1);
+            return c;
+        }
+
         static void Main(string[] args)
         {
-
-            int posA, posB, posAux1, posAux2, posAux3, posAux4, j, k;
-            bool a, b, c, d;
-            posA = 0;
-            posB = 0;
-            posAux1 = 0;
-            posAux2 = 0;
-            posAux3 = 0;
-
-            j = 0;
-            k = 0;
-
-            a = true;
-            b = true;
-            c = true;
-            d = true;
-           
-            Console.WriteLine("OPERACIONES LÓGICAS BÁSICAS POR MOISÉS Y HUGO\n\n");
-            Console.WriteLine("Presione una tecla...");
-            Console.ReadLine();
-            Console.WriteLine("Operación 1: (a ^ b) v ~b");
-            Console.ReadLine();
-            Console.WriteLine("Presione una tecla...");
-
-            
-            //Estos son los encabezados
-            WriteAt("a", 4, 9);
-            WriteAt("b", 10, 9);
-            WriteAt("(a ^ b)", 16, 9);
-            WriteAt("~b", 29, 9);
-            WriteAt("(a ^ b) v ~b", 36, 9);
-
-            posA = 9;
-            posB = 9;
-            posAux1 = 9;
-            posAux2 = 9;
-            posAux3 = 9;
-
-            for (j = 0; j <= 1; j++)
-            {
-                if (j == 0)
-                    a = false;
-                else
-                    a = true;
-
-                for (k = 0; k <= 1; k++)
-                {
-                    if (k == 0)
-                        b = false;
-                    else
-                        b = true;
-                    //Esto es a
-                    WriteAt(j.ToString(), 4, posA+=1);
-
-                    //Esto es b
-                    WriteAt(k.ToString(), 10, posB+=1);
-
-                    //Esto es (a ^ b)
-                    if (a && b)
-                        WriteAt("1", 19, posAux1 += 1);
-                    else
-                        WriteAt("0", 19, posAux1 += 1);
-
-                    //Esto es ~b
-                    if (b)
-                        WriteAt("0", 30, posAux2 += 1);
-                    else
-                        WriteAt("1", 30, posAux2 += 1);
-
-                   if(a || b)
-                        WriteAt("1", 42, posAux3 += 1);
-                    else
-                        WriteAt("0", 42, posAux3 += 1);
-
-
-                    Console.ReadLine();
-                }
-            }
-
-            WriteAt("Presione una tecla... ", 0, 15);
-            Console.ReadLine();
-
-            WriteAt("Operación 2: (a -> b) ^ ~(a v b)", 0, 17);
-            Console.ReadLine();
-
-
-            WriteAt("a", 4, 19);
-            WriteAt("b", 10, 19);
-            WriteAt("(a -> b)", 16, 19);
-            WriteAt("(a v b)", 29, 19);
-            WriteAt("~(a v b)", 40, 19);
-            WriteAt("(a -> b) ^ ~(a v b)", 53, 19);
-
-
-            Console.ReadLine();
-
-            
-            posA = 19;
-            posB = 19;
-            posAux1 = 19;
-            posAux2 = 19;
-            posAux3 = 19;
-            posAux4 = 19;
-            for (j = 0; j <= 1; j++)
-            {
-                if (j == 0)
-                    a = false;
-                else
-                    a = true;
-
-                for (k = 0; k <= 1; k++)
-                {
-                    if (k == 0)
-                        b = false;
-                    else
-                        b = true;
-                    //Esto es a
-                    WriteAt(j.ToString(), 4, posA += 1);
-
-                    //Esto es b
-                    WriteAt(k.ToString(), 10, posB += 1);
-
-                    //Esto es (a -> b)
-                    if (a == b)
-                    {
-                        WriteAt("1", 19, posAux1 += 1);
-                        c = true;
-                    }
-                    else
-                    {
-                        WriteAt("0", 19, posAux1 += 1);
-                        c = false;
-                    }
-                    
-                    //Esto es (a v b)
-                    if (a || b)
-                        WriteAt("1", 32, posAux2 += 1);
-                    else
-                        WriteAt("0", 32, posAux2 += 1);
-
-                    //Esto es ~(a v b)
-                    if (a || b)
-                    {
-                        WriteAt("0", 44, posAux3 += 1);
-                        d = false;
-                    }
-                    else
-                    {
-                        WriteAt("1", 44, posAux3 += 1);
-                        d = true;
-                    }
-
-                    //Esto es (a -> b) ^ ~(a v b)
-
-                    if (c && d)
-                        WriteAt("1", 64, posAux4 += 1);
-                    else
-                        WriteAt("0", 64, posAux4 += 1);
-
-
-
-                    Console.ReadLine();
-                }
-            }
-            
-
+            int n;
+            Console.WriteLine("Ingrese un número: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(permutacionSinRepeticion(n).ToString());
 
         }
     }
